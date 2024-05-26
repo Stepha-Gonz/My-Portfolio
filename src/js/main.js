0b1812e769e0d619b15232534032b04e2fbb0adf
Stephanie Gonzalez <gstephaniegonz@gmail.com>
2024-05-26 11:00:06 -0500

    style & feat: added navigation bar and styled it

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  showEvents();
+  darkMode();
+});
+
+function showEvents() {
+  const mobileMenu = document.querySelector(".mobile-menu");
+  mobileMenu.addEventListener("click", navegacionResponsive);
+
+  const closeMenu = document.querySelector(".nav__close");
+  closeMenu.addEventListener("click", cierreMenu);
+}
+
+function navegacionResponsive() {
+  const menuOptions = document.querySelector(".menu__hidden");
+  menuOptions.classList.toggle("show");
+}
+
+function cierreMenu() {
+  const menuClose2 = document.querySelector(".menu__hidden");
+  menuClose2.classList.remove("show");
+}
+
+function darkMode() {
+  const darkModeON = window.matchMedia("prefers-color-scheme:dark");
+  if (darkModeON.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+
+  darkModeON.addEventListener("change", function () {
+    if (darkModeON.matches) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.add("dark-mode");
+    }
+  });
+
+  const darkModeButtonON = document.querySelector(".dark-mode-button");
+  darkModeButtonON.addEventListener("click", changeDarkMode);
+}
+
+function changeDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
