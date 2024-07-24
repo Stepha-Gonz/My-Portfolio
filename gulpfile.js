b0206d9670f90191966291d13f00fefd41bd2ad2
Stephanie Gonzalez <gstephaniegonz@gmail.com>
2024-07-24 16:06:43 -0500

    configured webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,144 @@
 import path from "path";
 import fs from "fs";
 import { glob } from "glob";
-import { src, dest, watch, series, parallel } from "gulp";
+import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 import concat from "gulp-concat";
 import terser from "gulp-terser";
 import sharp from "sharp";
 import rename from "gulp-rename";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import webpack from "webpack-stream";
+import sourcemaps from "gulp-sourcemaps";
+import { deleteAsync } from "del";
 
 const sass = gulpSass(dartSass);
 
 const paths = {
   scss: "src/scss/**/*.scss",
   js: "src/js/**/*.js",
+  img: "src/img/**/*",
 };
 
-export function css(done) {
-  src(paths.scss, { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "compressed",
-      }).on("error", sass.logError)
-    )
-    .pipe(dest("./build/css", { sourcemaps: "." }));
-  done();
+function handleError(err) {
+  console.error(err.toString());
+  this.emit("end");
 }
 
-export function js(done) {
-  src(paths.js)
-    .pipe(concat("bundle.js"))
-    .pipe(terser())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(dest("./build/js"));
-  done();
+export function css() {
+  return src(paths.scss, { sourcemaps: true })
+    .pipe(sass({ outputStyle: "expanded" }).on("error", handleError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest("./public/build/css", { sourcemaps: "." }))
+    .on("error", handleError);
 }
 
-export async function imagenes(done) {
-  const srcDir = "./src/img";
-  const buildDir = "./build/img";
-  const images = await glob("./src/img/**/*");
-
-  images.forEach((file) => {
-    const relativePath = path.relative(srcDir, path.dirname(file));
-    const outputSubDir = path.join(buildDir, relativePath);
-    procesarImagenes(file, outputSubDir);
-  });
-  done();
+export function js() {
+  return (
+    src(paths.js, { sourcemaps: true })
+      .pipe(
+        webpack({
+          module: {
+            rules: [
+              {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
+            ],
+          },
+          mode: "production",
+          //.watch: true,
+          entry: "./src/js/app.js",
+        }).on("error", handleError)
+      )
+      //.pipe(concat("bundle.js").on("error", handleError))
+      .pipe(sourcemaps.init())
+      .pipe(terser().on("error", handleError))
+      .pipe(rename({ suffix: ".min" }).on("error", handleError))
+      .pipe(dest("./public/build/js", { sourcemaps: "." }))
+      .on("error", handleError)
+  );
 }
 
-function procesarImagenes(file, outputSubDir) {
-  if (!fs.existsSync(outputSubDir)) {
-    fs.mkdirSync(outputSubDir, { recursive: true });
+export async function imagenes() {
+  try {
+    const srcDir = "./src/img";
+    const buildDir = "./public/build/img";
+    const images = await glob(paths.img);
+
+    for (const file of images) {
+      const relativePath = path.relative(srcDir, path.dirname(file));
+      const outputSubDir = path.join(buildDir, relativePath);
+      await procesarImagenes(file, outputSubDir);
+    }
+  } catch (err) {
+    handleError.call(this, err);
   }
-  const baseName = path.basename(file, path.extname(file));
-  const extName = path.extname(file);
+}
 
-  if (extName.toLowerCase() === ".svg") {
-    const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
-    fs.copyFileSync(file, outputFile);
-  } else {
+async function procesarImagenes(file, outputSubDir) {
+  try {
+    if (!fs.existsSync(outputSubDir)) {
+      fs.mkdirSync(outputSubDir, { recursive: true });
+    }
+    const baseName = path.basename(file, path.extname(file));
+    const extName = path.extname(file).toLowerCase();
+
     const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
     const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
     const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`);
-    const options = { quality: 80 };
 
-    sharp(file).jpeg(options).toFile(outputFile);
-    sharp(file).webp(options).toFile(outputFileWebp);
-    sharp(file).avif().toFile(outputFileAvif);
+    if (extName === ".svg") {
+      fs.copyFileSync(file, outputFile);
+    } else if (extName === ".png") {
+      // Procesa PNGs sin perder la transparencia
+      await sharp(file).toFile(outputFile).catch(handleError);
+      await sharp(file)
+        .webp({ quality: 80, lossless: true })
+        .toFile(outputFileWebp)
+        .catch(handleError);
+      await sharp(file)
+        .avif({ quality: 80, lossless: true })
+        .toFile(outputFileAvif)
+        .catch(handleError);
+    } else {
+      const options = { quality: 80 };
+      await sharp(file).jpeg(options).toFile(outputFile).catch(handleError);
+      await sharp(file).webp(options).toFile(outputFileWebp).catch(handleError);
+      await sharp(file).avif(options).toFile(outputFileAvif).catch(handleError);
+    }
+  } catch (err) {
+    handleError.call(this, err);
+  }
+}
+
+async function eliminarImagenes(file) {
+  try {
+    const srcDir = "./src/img";
+    const buildDir = "./public/build/img";
+    const relativePath = path.relative(srcDir, path.dirname(file));
+    const baseName = path.basename(file, path.extname(file));
+    const extName = path.extname(file).toLowerCase();
+
+    const outputSubDir = path.join(buildDir, relativePath);
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`);
+
+    await deleteAsync([outputFile, outputFileWebp, outputFileAvif]);
+  } catch (err) {
+    handleError.call(this, err);
   }
 }
 
 export function dev() {
-  watch(paths.scss, css);
-  watch(paths.js, js);
-  watch("src/img/**/*.{png,jpg}", imagenes);
+  watch(paths.scss, css).on("error", handleError);
+  watch(paths.js, js).on("error", handleError);
+  const imgWatcher = watch(paths.img, imagenes).on("error", handleError);
+  imgWatcher.on("unlink", eliminarImagenes).on("error", handleError);
 }
 
-// Nueva tarea de construcción para producción
 export const build = series(js, css, imagenes);
-
 export default dev;
