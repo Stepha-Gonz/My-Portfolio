ddf9eb455610d42de6ac3d4ab2b0d582df74ea83
unknown <gstephaniegonz@gmail.com>
2025-06-25 22:42:05 -0500

    updated CV, experience, and projects up to june 25th

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./views/**/*.html", "./src/js/**/*.js", "./src/scss/**/*.scss"],
+  content: [
+    "./index.html",
+    "./views/**/*.html",
+    "./src/js/**/*.js",
+    "./src/scss/**/*.scss",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
